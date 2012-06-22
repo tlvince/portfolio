ad154c285f47f57cf1806ab063f47753027bc087
Tom Vincent <tlvince@gmail.com>
2012-06-22 20:33:32 +0100

    Enable gzip

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var port = process.env.PORT || 3000;
 var oneDay = 86400000;
 
 connect.createServer(
+  connect.compress(),
   connect.logger('short'),
   connect.favicon(__dirname + '/build/assets'),
   connect.static(__dirname + '/build', { maxAge: oneDay })
